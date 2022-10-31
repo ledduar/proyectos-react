dba4285316178dbdb71d65dc2bd5c04db7e01c67
ledduar <luiswxyz32@gmail.com>
2022-10-31 00:17:22 -0500

    first commit

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,22 +1,58 @@
+import { useState, useEffect } from "react";
 
 const Formulario = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validación de formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+      console.log('Hay al menos un campo vacio');
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="text-lg mt-5 text-center mb-5">
         Añade pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
-
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+        {error &&
+          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        }
         <div className="mb-5">
-          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre mascota</label>
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
+            Nombre mascota
+          </label>
           <input
             id="mascota"
             type="text"
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
 
@@ -27,6 +63,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre del Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div>
 
@@ -37,6 +75,8 @@ const Formulario = () => {
             type="email"
             placeholder="Email Contacto Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -46,6 +86,8 @@ const Formulario = () => {
             id="alta"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
 
@@ -55,6 +97,8 @@ const Formulario = () => {
             id="sintomas"
             placeholder="Describe los Sintomas"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
           />
         </div>
 
