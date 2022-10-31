aeb2c02abba4f2ee46bc438a2eebd2128ea17331
ledduar <luiswxyz32@gmail.com>
2022-10-30 22:45:41 -0500

    first commit

---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,30 @@
+
+const Paciente = () => {
+    return (
+
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">Luis</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                <span className="font-normal normal-case">24 de Mayo de 1996</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+                <span className="font-normal normal-case">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, modi eos! Accusantium voluptatem neque voluptas molestias ad, eos ratione perspiciatis necessitatibus sit, saepe eum laborum iusto non! Deleniti, sit ducimus.</span>
+            </p>
+        </div>
+    )
+}
+
+export default Paciente
